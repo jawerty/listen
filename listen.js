f8d4e69ae23dfac6cd559b52701c30c92077fc48
jawerty <jawerty210@gmail.com>
2013-05-18 17:36:13 -0400

    Change UI, mroe simplistic

--- listen.js
+++ listen.js
@@ -1,58 +1,69 @@
+var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
+ 'runtime' : 'extension';
+
 $("#controls").css("display", "none")
 
-url="http://google.com"; 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  chrome.tabs.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.action == 'open_dialog_box') {
-      alert("Message recieved!");
-    }
+/*
+to do:
+=end and start button in content script
+=create audio tag in background
+=
+=
+*/
+var msgRemoved;
+function wrap(){
+  chrome.extension.sendMessage({text:"startListen"},function(response){
+    $(function main(){      
+      $('#play').mousedown(function(e){
+        $("#play").attr('src',"images/play_clicked.png");
+        return false;
+      }).mouseup(function(e){
+        $("#body_text").css("display", "none");
+        $("#play").css("display", "none");
+        $("#play").attr('src',"images/play.png");
+        $("#pause").css("display", "block");
+        $("#pause").attr('src',"images/pause.png");
+        $("#loading").css("display", "block")
+        $("#loading").css("-webkit-animation-play-state", "running");
+
+        if (msgRemoved != true) $("#play_div").append("<label id='message_4'>hint: Hold pause to stop</label>")
+        
+        chrome.extension.sendMessage({text:"play"});
+        return false; 
+      });
+
+      $('#pause').mousedown(function(e){    
+        $("#pause").attr('src',"images/pause_clicked.png");
+
+        this.timeout = setTimeout(function() {
+            $("#pause").css("display", "none");
+            $("#play").css("display", "block");
+            $("#play").attr('src',"images/play.png");
+            $("#loading").css("display", "none");
+            chrome.extension.sendMessage({text:"stop"});
+        }, 2000);
+
+        return false;
+      }).mouseup(function(e){
+
+        clearTimeout(this.timeout);
+        if (typeof msgRemoved == 'undefined') {
+          $("#message_4").remove();
+          msgRemoved = true;
+        }
+
+        $("#pause").css("display", "none");
+        $("#play").css("display", "block");
+        $("#play").attr('src',"images/play.png");
+        $("#loading").css("-webkit-animation-play-state", "paused");
+
+        chrome.extension.sendMessage({text:"pause"});
+        return false; 
+      });
+    });
+
+    chrome.extension.sendMessage({text:"restartListen"});
   });
-});
+}
 
-$(function main(){
-
-    function listen(text,vol,loop,delay)
-    {
-        var child;
-        if (loop == true) looped = true;
-        else looped = false;
-        vol=vol || 1;   
-        delay=delay || 0;
-        var text_mod = text.replace(' ', '+')
-        api="http://tts-api.com/tts.mp3?q="+text_mod;
-
-        setTimeout(function(){
-            child = document.createElement("audio");
-            child.setAttribute("src",api);
-            child.volume=vol;
-            child.load();
-            child.setAttribute("autoplay","autoplay");
-            if (looped) child.setAttribute("loop","loop");
-            child.setAttribute("controls", "controls");
-            child.setAttribute("style", "margin: 30px 0px;");
-            child.addEventListener("load", function() {
-                child.play();
-            });
-
-            child.addEventListener("ended", function() {
-                child.remove()
-                $("#play").css("display", "block")
-            })
-            $("#controls").css("display", "block")
-            document.getElementById("controls").appendChild(child);
-        }, delay);
-    };
-    
-    $(function() {
-     $('#play').mousedown(function(){
-       $("#play").attr('src',"images/play_clicked.png");
-       return false;
-     }).mouseup(function(){
-       $("#body_text").css("display", "none")
-       $("#play").css("display", "none")
-       $("#play").attr('src',"images/play.png");
-       listen("Hello World. My name is Jared Wright and this is listen for chrome. Listen is a text to speech app for 'listening' to the text of any website at the click of a button", 1)
-       return false; 
-     })
-    })
-});
+wrap()
