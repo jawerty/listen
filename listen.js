a3c9ed607ca08b510f3e5be3dad599b96ca14de3
jawerty <jawerty210@gmail.com>
2013-05-18 21:27:47 -0400

    Semi-working version, no web scraping yet

--- listen.js
+++ listen.js
@@ -1,69 +1,77 @@
 var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
  'runtime' : 'extension';
 
-$("#controls").css("display", "none")
-
 /*
 to do:
-=end and start button in content script
-=create audio tag in background
-=
-=
+
+=optimize play and pause button
+=create website 
+
 */
+var bg = chrome.extension.getBackgroundPage();
+
 var msgRemoved;
-function wrap(){
-  chrome.extension.sendMessage({text:"startListen"},function(response){
-    $(function main(){      
-      $('#play').mousedown(function(e){
-        $("#play").attr('src',"images/play_clicked.png");
-        return false;
-      }).mouseup(function(e){
-        $("#body_text").css("display", "none");
-        $("#play").css("display", "none");
-        $("#play").attr('src',"images/play.png");
-        $("#pause").css("display", "block");
-        $("#pause").attr('src',"images/pause.png");
-        $("#loading").css("display", "block")
-        $("#loading").css("-webkit-animation-play-state", "running");
 
-        if (msgRemoved != true) $("#play_div").append("<label id='message_4'>hint: Hold pause to stop</label>")
-        
-        chrome.extension.sendMessage({text:"play"});
-        return false; 
-      });
+chrome.extension.sendMessage({text:"startListen"},function(response){
+  child = bg.document.getElementById('listen');
+  
+  $(function main(){ 
+    $('#play').mousedown(function(e){
+      $("#play").attr('src',"images/play_clicked.png");
+      return true;
+    }).mouseup(function(e){
+      $("#body_text").css("display", "none");
+      $("#play").css("display", "none");
+      $("#play").attr('src',"images/play.png");
+      $("#pause").css("display", "block");
+      $("#pause").attr('src',"images/pause.png");
+      $("#loading").css("display", "block");
+      $("#loading").css("-webkit-animation-play-state", "running");
 
-      $('#pause').mousedown(function(e){    
-        $("#pause").attr('src',"images/pause_clicked.png");
+      if (msgRemoved != true) $("#play_div").append("<label id='message_4'>hint: Hold pause to stop</label>")
+      
+      chrome.extension.sendMessage({text:"play"});
+      return true; 
+    });
 
-        this.timeout = setTimeout(function() {
-            $("#pause").css("display", "none");
-            $("#play").css("display", "block");
-            $("#play").attr('src',"images/play.png");
-            $("#loading").css("display", "none");
-            chrome.extension.sendMessage({text:"stop"});
-        }, 2000);
-
-        return false;
-      }).mouseup(function(e){
-
-        clearTimeout(this.timeout);
+    $('#pause').mousedown(function(e){    
+      $("#pause").attr('src',"images/pause_clicked.png");
+      timeout = setTimeout(function() {
+        var filler = true;
         if (typeof msgRemoved == 'undefined') {
           $("#message_4").remove();
           msgRemoved = true;
         }
-
         $("#pause").css("display", "none");
-        $("#play").css("display", "block");
-        $("#play").attr('src',"images/play.png");
-        $("#loading").css("-webkit-animation-play-state", "paused");
+        $("#loading").css("margin-top", "20px");
+        setTimeout(function(){
+          $("#loading").css("display", "none")
+          $("#play").css("display", "block");
+          $("#play").attr('src',"images/play.png");
+          $("#loading").css("margin-top", "0px");
+        }, 2000)
+        chrome.extension.sendMessage({text:"stop"});
+      }, 2000);
+      
+      return true;
 
-        chrome.extension.sendMessage({text:"pause"});
-        return false; 
-      });
+    }).mouseup(function(e){
+      clearTimeout(timeout);
+       
+      $("#pause").css("display", "none");
+      $("#play").css("display", "block");
+      $("#play").attr('src',"images/play.png");
+      $("#loading").css("-webkit-animation-play-state", "paused");
+      chrome.extension.sendMessage({text:"pause"});   
+    
+      if (typeof msgRemoved == 'undefined') {
+        $("#message_4").remove();
+        msgRemoved = true;
+      }
+      return true; 
     });
-
-    chrome.extension.sendMessage({text:"restartListen"});
+    
   });
-}
+  chrome.extension.sendMessage({text:"restartListen"});
+});
 
-wrap()
