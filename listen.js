28a0dd341dc8599935fa28d42e308e6f8f3369e8
jawerty <jawerty210@gmail.com>
2013-05-19 19:31:37 -0400

    First working version

--- listen.js
+++ listen.js
@@ -4,18 +4,34 @@ var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
 /*
 to do:
 
-=optimize play and pause button
+=make persistent connection
+=get content from website
+=read content from website
 =create website 
 
 */
-var bg = chrome.extension.getBackgroundPage();
-
+var ended; 
 var msgRemoved;
+var port = chrome[runtimeOrExtension].connect({name: "listen_port"});
 
-chrome.extension.sendMessage({text:"startListen"},function(response){
-  child = bg.document.getElementById('listen');
-  
-  $(function main(){ 
+port.postMessage({text:"startListen"});
+
+port.onMessage.addListener(function(msg){
+  if (msg.ended == true) {
+    $("#message_4").remove();
+    ended = true
+    msgRemoved = true
+
+    $("#loading").css("display", "none");
+    $("#play").css("display", "block");
+    $("#pause").css("display", "none");
+    $("#body_text").css("display", "none");
+  }
+  if (msg.text == 'listening...'){
+    main(ended);
+  }
+
+  function main(ended){  
     $('#play').mousedown(function(e){
       $("#play").attr('src',"images/play_clicked.png");
       return true;
@@ -30,48 +46,51 @@ chrome.extension.sendMessage({text:"startListen"},function(response){
 
       if (msgRemoved != true) $("#play_div").append("<label id='message_4'>hint: Hold pause to stop</label>")
       
-      chrome.extension.sendMessage({text:"play"});
+      port.postMessage({text:"play"});
       return true; 
     });
 
     $('#pause').mousedown(function(e){    
       $("#pause").attr('src',"images/pause_clicked.png");
-      timeout = setTimeout(function() {
-        var filler = true;
+      this.timeout = setTimeout(function() {
         if (typeof msgRemoved == 'undefined') {
           $("#message_4").remove();
           msgRemoved = true;
         }
+
         $("#pause").css("display", "none");
         $("#loading").css("margin-top", "20px");
+
         setTimeout(function(){
           $("#loading").css("display", "none")
           $("#play").css("display", "block");
           $("#play").attr('src',"images/play.png");
           $("#loading").css("margin-top", "0px");
-        }, 2000)
-        chrome.extension.sendMessage({text:"stop"});
+        }, 2000);
+
+        port.postMessage({text:"stop"});
+
       }, 2000);
-      
       return true;
 
     }).mouseup(function(e){
-      clearTimeout(timeout);
+      this.clearTimeout(timeout);
        
       $("#pause").css("display", "none");
       $("#play").css("display", "block");
       $("#play").attr('src',"images/play.png");
       $("#loading").css("-webkit-animation-play-state", "paused");
-      chrome.extension.sendMessage({text:"pause"});   
+
+      port.postMessage({text:"pause"});   
     
       if (typeof msgRemoved == 'undefined') {
         $("#message_4").remove();
         msgRemoved = true;
       }
-      return true; 
+
+      return true;
     });
     
-  });
-  chrome.extension.sendMessage({text:"restartListen"});
+  }
 });
 
