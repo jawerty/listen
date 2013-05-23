de7b893b86e0e7436e48434ec65bd4b829c14865
jawerty <jawerty210@gmail.com>
2013-05-23 18:18:22 -0400

    recent changes

--- listen.js
+++ listen.js
@@ -4,93 +4,150 @@ var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
 /*
 to do:
 
-=make persistent connection
-=get content from website
-=read content from website
+=get pdf working just like any other website
+=optimize gui
+=make indexing faster
 =create website 
 
 */
+
 var ended; 
 var msgRemoved;
+var msgAdded = false;
+var pdfFinishMsg = false;
+var stopAdded;
 var port = chrome[runtimeOrExtension].connect({name: "listen_port"});
 
 port.postMessage({text:"startListen"});
 
+var bkg = chrome.extension.getBackgroundPage();
+
 port.onMessage.addListener(function(msg){
+  if (msg.pdf == true){
+
+    if (msgAdded == false) {
+      $("#play_div").append("<p id='message_4'>Long PDFs (books) may take a few minutes to load.</p>")
+      msgAdded = true;
+    }
+    
+    $("#play").css("display", "none");
+    $("#loading").css("display", "block");
+    $("#loading").css("-webkit-animation-play-state", "running");
+
+  }
+
+  if (msg.pdf == 'finished'){
+    $("#message_4").remove();
+    if (pdfFinishMsg == false){
+      $("#play_div").append("<p id='message_5'>PDF finished loading...Now playing</p>");
+      pdfFinishMsg = true;
+    }
+    
+    if (stopAdded != true) {
+      $("#play_div").append("<p id='stop'>stop</p>");
+      stopAdded = true;
+    }
+
+    $("#play").css("display", "none");
+    $("#pause").css("display", "block");
+    $("#loading").css("display", "block");
+    $("#loading").css("-webkit-animation-play-state", "running");
+    port.postMessage({text:"play"});
+  }
+
   if (msg.ended == true) {
     $("#message_4").remove();
+    $("#message_5").remove();
+    $("#stop").remove();
+
+    stopAdded = false;
     ended = true
-    msgRemoved = true
+    msgAdded = true;
+    pdfFinishMsg = false;
 
     $("#loading").css("display", "none");
     $("#play").css("display", "block");
     $("#pause").css("display", "none");
-    $("#body_text").css("display", "none");
   }
+
   if (msg.text == 'listening...'){
     main(ended);
   }
 
+  if (msg.text == 'playing') {
+    $("#loading").css("display", "block");
+    $("#loading").css("-webkit-animation-play-state", "running");
+    $("#play").css("display", "none");
+    $("#pause").css("display", "block");
+  }
+
+  if (msg.text == 'paused') {
+    $("#loading").css("display", "block");
+    $("#loading").css("-webkit-animation-play-state", "paused");
+    $("#play").css("display", "block");
+    $("#pause").css("display", "none");
+  }
+
   function main(ended){  
     $('#play').mousedown(function(e){
+      $("#pause").css("display", "none");
       $("#play").attr('src',"images/play_clicked.png");
       return true;
+
     }).mouseup(function(e){
-      $("#body_text").css("display", "none");
       $("#play").css("display", "none");
       $("#play").attr('src',"images/play.png");
       $("#pause").css("display", "block");
       $("#pause").attr('src',"images/pause.png");
       $("#loading").css("display", "block");
       $("#loading").css("-webkit-animation-play-state", "running");
-
-      if (msgRemoved != true) $("#play_div").append("<label id='message_4'>hint: Hold pause to stop</label>")
       
+      if (stopAdded != true) {
+        $("#play_div").append("<p id='stop'>stop</p>")
+        stopAdded = true;
+      }
+
       port.postMessage({text:"play"});
       return true; 
     });
 
-    $('#pause').mousedown(function(e){    
+    $('#pause').mousedown(function(e){   
       $("#pause").attr('src',"images/pause_clicked.png");
-      this.timeout = setTimeout(function() {
-        if (typeof msgRemoved == 'undefined') {
-          $("#message_4").remove();
-          msgRemoved = true;
-        }
 
-        $("#pause").css("display", "none");
-        $("#loading").css("margin-top", "20px");
-
-        setTimeout(function(){
-          $("#loading").css("display", "none")
-          $("#play").css("display", "block");
-          $("#play").attr('src',"images/play.png");
-          $("#loading").css("margin-top", "0px");
-        }, 2000);
-
-        port.postMessage({text:"stop"});
-
-      }, 2000);
-      return true;
+      if (msgAdded == true) {
+        $("#message_4").remove();
+        msgAdded = false;
+      }
 
     }).mouseup(function(e){
-      this.clearTimeout(timeout);
-       
+      if (pdfFinishMsg == true) {
+        $("#message_5").remove();
+        pdfFinishMsg = false;
+      }
+      
+      $("#pause").attr('src',"images/pause.png");
       $("#pause").css("display", "none");
       $("#play").css("display", "block");
       $("#play").attr('src',"images/play.png");
       $("#loading").css("-webkit-animation-play-state", "paused");
-
       port.postMessage({text:"pause"});   
-    
-      if (typeof msgRemoved == 'undefined') {
+      
+      if (msgAdded == true) {
         $("#message_4").remove();
-        msgRemoved = true;
+        msgAdded = false;
       }
 
-      return true;
     });
     
+    $('#stop').click(function(e){
+      $("#pause").css("display", "none");
+      $("#play").css("display", "block");
+      $("#play").attr('src',"images/play.png");
+      $("#loading").css("display", "none");
+      port.postMessage({text:"stop"});
+      $("#stop").remove();  
+      stopAdded = false;
+    });  
   }
 });
 
