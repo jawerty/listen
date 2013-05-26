4cfd85b98e457cb071efde6be004bc22428abe36
jawerty <jawerty210@gmail.com>
2013-05-26 17:55:21 -0400

    Website changes

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+console.log('audio');
+
 text = "Listen is a text to speech app for hearing the text on any website. \
 By the click of a button, the website's text is generated and played through the \
-tts-api. Another feature the Listen app has it the ability to read pdf pages. \
+tts-api. Another feature the Listen app has is the ability to read pdf pages. \
 This feature is in beta and somewhat delicate for production, so try not to read \
 pdf websites more than 5-10 pages long. For developers reading this, the source code \
-is hosted here on Github. Enjoy...";
+is hosted here on Git hub. Enjoy...";
 
 api="http://tts-api.com/tts.mp3?q="+text;
 child = document.createElement("audio");
@@ -12,16 +14,16 @@ child.load();
 child.volume=1;
 
 child.addEventListener("ended", function(e){
-  $("#play").src = 'images/play_large.png';
+  $("#play").src = 'images/play.png';
   $("#pause").css('display', 'none');
-  $("#play").css('display', 'block');
+  $("#play").css('display', 'inline');
 });
 $("#play").mousedown(function(e){
 	$("#play").src = 'images/play_clicked.png';
 }).mouseup(function(e){
   child.play()
 	$("#play").css('display', 'none');
-	$("#pause").css('display', 'block');
+	$("#pause").css('display', 'inline');
 })
 
 $('#pause').mousedown(function(e){
@@ -29,6 +31,6 @@ $('#pause').mousedown(function(e){
 }).mouseup(function(e){
 	child.pause();
   $("#pause").css('display', 'none');
-  $("#play").src = 'images/play_large.png';
-  $("#play").css('display', 'block');
+  $("#play").src = 'images/play.png';
+  $("#play").css('display', 'inline');
 })
