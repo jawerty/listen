0d8ccf449785ddb6394064209f8298d30f7d3a43
jawerty <jawerty210@gmail.com>
2013-05-26 16:51:28 -0400

    Website changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $("#play").mousedown(function(e){
-	$("#play").src = '../images/play_clicked.png';
+	$("#play").src = 'images/play_clicked.png';
 }).mouseup(function(e){
-  api="http://tts-api.com/tts.mp3?q="+text_mod;
+  api="http://tts-api.com/tts.mp3?q="+$('#readThis').innerText;
   child = document.createElement("audio");
   child.setAttribute("src",api);
   child.load(); //reload the source (the source changes a lot)
@@ -10,7 +10,7 @@ $("#play").mousedown(function(e){
   //event listener for when the audio ends
   //...mainly for control flow and a more intuitive UI
   child.addEventListener("ended", function(e){
-  	$("#play").src = '../images/play.png';
+  	$("#play").src = '..images/play.png';
   	$("#pause").css('display', 'none');
   	$("#play").css('display', 'block');
   });
@@ -20,7 +20,7 @@ $("#play").mousedown(function(e){
 })
 
 $('#pause').mousedown(function(e){
-	$("#pause").src = '../images/pause_clicked.png';
+	$("#pause").src = 'images/pause_clicked.png';
 }).mouseup(function(e){
 	child.pause();
 })
