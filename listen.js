0fc71eaa7285e661b47262652f17a25f665c1d3c
jawerty <jawerty210@gmail.com>
2013-05-26 14:20:58 -0400

    Working version w/ commented code

--- listen.js
+++ listen.js
@@ -1,35 +1,49 @@
+/*
+listen.js
+
+MIT License
+Copyright (c) 2013
+ 
+author: Jared Wright
+email: jawerty210@gmail.ocm
+
+description: content_script code for listen extension
+*/
+
+//Usage for chrome optimization based on runtime || extension
 var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
  'runtime' : 'extension';
 
-/*
-to do:
-
-=get pdf working just like any other website
-=optimize gui
-=make indexing faster
-=create website 
-
-*/
-
+//event variables
+var mainSent = false;
 var ended; 
 var msgRemoved;
 var msgAdded = false;
 var pdfFinishMsg = false;
 var stopAdded;
-var port = chrome[runtimeOrExtension].connect({name: "listen_port"});
+var port = chrome[runtimeOrExtension].connect({name: "listen_port"}); //port variable
 
 port.postMessage({text:"startListen"});
 
+//so I can log messages in the background console
 var bkg = chrome.extension.getBackgroundPage();
 
 port.onMessage.addListener(function(msg){
+  //initiated the UI
+  if (msg.text == 'listening...'){
+    main(ended);
+  }
+
+  //if the url is a pdf
   if (msg.pdf == true){
 
     if (msgAdded == false) {
+      //for when it's still loading
       $("#play_div").append("<p id='message_4'>Long PDFs (books) may take a few minutes to load.</p>")
       msgAdded = true;
     }
     
+    //UI stuff, I won't get into this since it's self explanatory
     $("#play").css("display", "none");
     $("#loading").css("display", "block");
     $("#loading").css("-webkit-animation-play-state", "running");
@@ -37,9 +51,9 @@ port.onMessage.addListener(function(msg){
   }
 
   if (msg.pdf == 'finished'){
-    $("#message_4").remove();
+    $("#message_4").remove(); //get rid of the loading screen
     if (pdfFinishMsg == false){
-      $("#play_div").append("<p id='message_5'>PDF finished loading...Now playing</p>");
+      $("#play_div").append("<p id='message_5'>PDF finished loading...</p>");
       pdfFinishMsg = true;
     }
     
@@ -55,6 +69,7 @@ port.onMessage.addListener(function(msg){
     port.postMessage({text:"play"});
   }
 
+  //when any audio is finished remove these messages and change the UI accordingly
   if (msg.ended == true) {
     $("#message_4").remove();
     $("#message_5").remove();
@@ -70,25 +85,39 @@ port.onMessage.addListener(function(msg){
     $("#pause").css("display", "none");
   }
 
-  if (msg.text == 'listening...'){
-    main(ended);
-  }
-
+  //when the background knows it's playing, do this.
   if (msg.text == 'playing') {
     $("#loading").css("display", "block");
     $("#loading").css("-webkit-animation-play-state", "running");
     $("#play").css("display", "none");
     $("#pause").css("display", "block");
+    if (stopAdded != true) {
+      $("#play_div").append("<p id='stop'>stop</p>")
+      stopAdded = true;
+    }
   }
 
+  //when the background knows it's paused, do this.
   if (msg.text == 'paused') {
     $("#loading").css("display", "block");
     $("#loading").css("-webkit-animation-play-state", "paused");
     $("#play").css("display", "block");
     $("#pause").css("display", "none");
+    if (stopAdded != true) {
+      $("#play_div").append("<p id='stop'>stop</p>")
+      stopAdded = true;
+    }
   }
 
-  function main(ended){  
+  //function for intiaiting the UI
+  function main(ended){
+    //so the main function isn't being executed hundreds of times  
+    if (mainSent != true) {
+      port.postMessage({text:'main'});
+      mainSent = true;
+    }
+
+    //UI stuff to do when the mouse clicks the play button
     $('#play').mousedown(function(e){
       $("#pause").css("display", "none");
       $("#play").attr('src',"images/play_clicked.png");
@@ -107,10 +136,12 @@ port.onMessage.addListener(function(msg){
         stopAdded = true;
       }
 
+      //sends the message to play
       port.postMessage({text:"play"});
       return true; 
     });
 
+    //UI stuff to do when the mouse clicks the pause button
     $('#pause').mousedown(function(e){   
       $("#pause").attr('src',"images/pause_clicked.png");
 
@@ -130,7 +161,7 @@ port.onMessage.addListener(function(msg){
       $("#play").css("display", "block");
       $("#play").attr('src',"images/play.png");
       $("#loading").css("-webkit-animation-play-state", "paused");
-      port.postMessage({text:"pause"});   
+      port.postMessage({text:"pause"}); //tells background that the audio should be paused   
       
       if (msgAdded == true) {
         $("#message_4").remove();
@@ -138,16 +169,18 @@ port.onMessage.addListener(function(msg){
       }
 
     });
-    
+
+    //UI stuff to do when the mouse clicks the stop button
     $('#stop').click(function(e){
       $("#pause").css("display", "none");
       $("#play").css("display", "block");
       $("#play").attr('src',"images/play.png");
       $("#loading").css("display", "none");
-      port.postMessage({text:"stop"});
+      port.postMessage({text:"stop"}); //tells background page that the stop button was clicked
       $("#stop").remove();  
       stopAdded = false;
     });  
   }
 });
+
 
